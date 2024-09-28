d5f6fd18ea3ca5b0a08da908e33f121b499c7da4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-28 17:16:08 -0400

    Refactor cors middleware to  add support for cookies

--- middleware/cors.js
+++ middleware/cors.js
@@ -12,14 +12,17 @@ const cors = (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', origin || true)
   } else {
     console.log('Origin not allowed:', origin)
+    throw new Error(`[${origin}] - Not allowed by CORS`)
   }
 
+  res.setHeader('Access-Control-Allow-Credentials', 'true') // allow cookies
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, DELETE, OPTIONS'
   )
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 
+  // OPTIONS method success status
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200)
   }
