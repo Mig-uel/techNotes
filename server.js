3e4382827dcb735a4f84dfbdeb75edf85d2bc77d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-28 01:08:48 -0400

    Refactor server.js to use a separate router import for the root endpoint

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const express = require('express')
 
+// routers
+const rootRouter = require('./routes/root')
+
 // init express instance
 const app = express()
 const port = process.env.PORT || 3500
@@ -8,7 +11,7 @@ const port = process.env.PORT || 3500
 // static folder
 app.use(express.static(path.resolve(__dirname, 'public')))
 
-app.use(require('./routes/root'))
+app.use(rootRouter)
 
 app.listen(port, () => {
   console.log(`SERVER RUNNING ON PORT: ${port}`)
