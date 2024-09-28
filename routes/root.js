042db474936875d3a3871471a4adc4e61ae70445
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-28 18:41:07 -0400

    Refactor route handler to use underscore (_) as a convention for unused variables

--- routes/root.js
+++ routes/root.js
@@ -4,7 +4,7 @@ const express = require('express')
 const router = express.Router()
 
 // route - / | /index | /index.html
-router.get('^/$|/index(.html)?', (req, res) => {
+router.get('^/$|/index(.html)?', (_, res) => {
   return res.sendFile(path.resolve(__dirname, '..', 'views', 'index.html'))
 })
 
