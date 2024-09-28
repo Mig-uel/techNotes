51b45564e8cd7b5ec214e5293b01c1f051d00166
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-28 01:19:40 -0400

    Refactor server.js to handle 404 errors with proper response and file rendering

--- server.js
+++ server.js
@@ -14,6 +14,24 @@ app.use(express.static(path.resolve(__dirname, 'public')))
 // routes
 app.use(rootRouter)
 
+// 404
+app.all('*', (req, res) => {
+  res.status(404)
+
+  // html request
+  if (req.accepts('html')) {
+    return res.sendFile(path.resolve(__dirname, 'views', '404.html'))
+  }
+  // json request
+  else if (req.accepts('json')) {
+    return res.json({ message: '404 Not Found' })
+  }
+  // if html or json was not matched in the accepts header
+  else {
+    res.type('txt').send('404 Not Found')
+  }
+})
+
 app.listen(port, () => {
   console.log(`SERVER RUNNING ON PORT: ${port}`)
 })
