e06c3fdb11d7c1dd1be0325e68d8eee367028a5a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-28 18:20:45 -0400

    Add User model with required fields and default values

---
+++ models/user.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    roles: {
+      type: String,
+      enum: ['employee', 'admin'],
+      default: 'employee',
+    },
+
+    active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('User', UserSchema)
