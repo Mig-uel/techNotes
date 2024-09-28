1a6a19a2cef02e1c2bc6a7b0e3f71b86c1baa315
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-28 19:29:32 -0400

    Refactor getAllUser and createNewUser in users.controller.js

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -8,14 +8,71 @@ const { asyncHandler } = require('../utils/asyncHandler')
  * @route GET /users
  * @access Private
  */
-const getAllUser = asyncHandler(async (req, res) => {})
+const getAllUser = asyncHandler(async (req, res) => {
+  const users = await User.find().select('-password').lean()
+
+  if (!users) {
+    const error = new Error('No users found!')
+    error.status = 404
+
+    throw error
+  }
+
+  return res.status(200).json(users)
+})
 
 /**
  * @desc Create new user
  * @route POST /users
  * @access Private
  */
-const createNewUser = asyncHandler(async (req, res) => {})
+const createNewUser = asyncHandler(async (req, res) => {
+  const { username, password, roles } = req.body
+
+  // confirm data
+  if (!username || !password || !Array.isArray(roles) || !roles.length) {
+    const error = new Error('All fields are required')
+    error.status = 400
+
+    throw error
+  }
+
+  // check for duplicate
+  const isDuplicate = await User.findOne({
+    username,
+  })
+    .lean()
+    .exec()
+
+  if (isDuplicate) {
+    const error = new Error('Username already exists')
+    error.status = 409
+
+    throw error
+  }
+
+  // hash password
+  const salt = 10
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  const userObject = {
+    username,
+    password: hashedPassword,
+    roles,
+  }
+
+  // create and store new user
+  const user = await User.create(userObject)
+
+  if (!user) {
+    const error = new Error('Invalid user data received')
+    error.status = 400
+
+    throw error
+  }
+
+  return res.status(200).json({ message: `${user.username} created` })
+})
 
 /**
  * @desc Update a user
