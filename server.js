9622293b5658b608d6caba3ebdbe5fe70ddb39bf
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-28 00:59:13 -0400

    Add static folder to serve static files

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+const path = require('path')
 const express = require('express')
 
+// init express instance
 const app = express()
 const port = process.env.PORT || 3500
 
+// static folder
+app.use(express.static(path.resolve(__dirname, 'public')))
+
 app.listen(port, () => {
-  console.log(`Server running on port: ${port}`)
+  console.log(`SERVER RUNNING ON PORT: ${port}`)
 })
