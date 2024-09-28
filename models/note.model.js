8a098cf73860254841df462802bb3efc7bbf4321
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-28 18:27:53 -0400

    Add mongoose-sequence-generator for auto-incrementing ticket numbers in Note model

--- models/note.model.js
+++ models/note.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const AutoIncrement = require('mongoose-sequence-generator')(mongoose)
 
 const NoteSchema = new mongoose.Schema(
   {
@@ -28,5 +29,11 @@ const NoteSchema = new mongoose.Schema(
     timestamps: true,
   }
 )
+
+NoteSchema.plugin(AutoIncrement, {
+  inc_field: 'ticket',
+  id: 'ticketNums',
+  start_seq: 500,
+})
 
 module.exports = mongoose.model('Note', NoteSchema)
