64bd69da290cafaa239e97c2a18b16132885219f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-28 01:22:50 -0400

    Add comment explaining the router path

--- routes/root.js
+++ routes/root.js
@@ -3,6 +3,7 @@ const express = require('express')
 
 const router = express.Router()
 
+// route - / | /index | /index.html
 router.get('^/$|/index(.html)?', (req, res) => {
   return res.sendFile(path.resolve(__dirname, '..', 'views', 'index.html'))
 })
